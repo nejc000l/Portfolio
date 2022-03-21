9459ee6b2799590c9bfbc8ea6fd3a7a0c02c7b00
nejc000l <nejc.lampic@gmail.com>
2022-03-21 01:59:37 +0100

    icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image'
 import React from 'react';
 import { AiFillGithub } from '@react-icons/all-files/Ai/AiFillGithub';
 import { SiNetlify } from '@react-icons/all-files/Si/SiNetlify';
@@ -10,12 +11,12 @@ function Header() {
       <div className="text-white flex justify-center">
         <div className='flex justify-center p-2	'>
         <a className='z-100' href="https://github.com/nejc000l?tab=repositories">
-        <AiFillGithub className='z-0 cursor-pointer p-2 text-[70px] duration-300  ease-in hover:text-black'>
+        <img src="images/gith.png" className='z-0 h-[5rem] cursor-pointer p-2 text-[70px] duration-300  ease-in hover:text-black'>
          
-          </AiFillGithub>   </a>
+          </img>   </a>
         </div>
         <div className='flex justify-center p-2	'>
-          <a href="https://app.netlify.com/teams/nejc000l/sites"><SiNetlify className='cursor-pointer p-2 text-[70px] duration-300 ease-in hover:text-black' /></a>
+          <a href="https://app.netlify.com/teams/nejc000l/sites"><img src="images/netlfiy.png"className='cursor-pointer h-[5rem] p-2 text-[70px] duration-300 ease-in hover:text-black' /></a>
         </div>
       </div>
     <Projects/>
